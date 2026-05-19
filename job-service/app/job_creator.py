5ef1bd1b854cd05062a33f6bf3314731ee121f46
Simon <schmalfusssimon95@gmail.com>
2026-05-19 19:52:43 +0200

    fixed return type

--- job-service/app/job_creator.py
+++ job-service/app/job_creator.py
@@ -3,7 +3,7 @@ import uuid
 
 class JobCreator: 
 
-    def create_job(self, text: str) -> tuple[int, str]: 
+    def create_job(self, text: str) -> tuple[str, str]: 
         job_id = str(uuid.uuid4())
         job = json.dumps({"job_id": job_id, "text": text}) 
         return job_id, job
