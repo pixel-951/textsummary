b2204036af2533196d8d8da977ab4ce26fa3e25d
Simon <schmalfusssimon95@gmail.com>
2026-05-19 14:36:03 +0200

    removed job_id attribute

--- model-service/app/job_processor.py
+++ model-service/app/job_processor.py
@@ -9,15 +9,14 @@ class JobProcessor:
     def __init__(self, summarizer: Summarizer, notifier:Notifier): 
         self.summarizer = summarizer
         self.notifier = notifier
-        self.current_jobid = 0
 
     def process_job(self, data: json) -> str: 
         loaded = json.loads(data.decode())
         # validation etc..
-        self.current_jobid = loaded['job_id']
+        job_id = loaded['job_id']
         summary = self.summarizer.summarize_text(loaded['text'])
-        self.notify(summary=summary)
+        self.notify(job_id=job_id, summary=summary)
         return summary
     
-    def notify(self, summary: str): 
-        self.notifier.notify(job_id=self.current_jobid, summary=summary)
+    def notify(self, job_id:int, summary: str): 
+        self.notifier.notify(job_id=job_id, summary=summary)
